bbe77bf4247e6ae210c15d67bbc7f61cc1ee9aec
mak <romanmak955@gmail.com>
2020-05-05 15:36:34 +0300

    init

---
+++ src/main/java/com/example/Limonservice/LimonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Limonservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimonServiceApplication.class, args);
+	}
+
+}
