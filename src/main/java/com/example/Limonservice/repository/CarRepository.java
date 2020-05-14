3f01a214033ec157a74a8a0a11616469965bda27
mak <romanmak955@gmail.com>
2020-05-14 12:27:25 +0300

    add UserRepository and CarRepository

---
+++ src/main/java/com/example/Limonservice/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package com.example.Limonservice.repository;
+
+import com.example.Limonservice.entity.CarEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<CarEntity, Long> {
+}
---
+++ src/main/java/com/example/Limonservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Limonservice.repository;
+
+import com.example.Limonservice.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
